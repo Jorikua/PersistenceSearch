fa333551c86336dea5d6b783006ebc7b60da2719
Vsevolod Kaganovych <kaganovychv@gmail.com>
2015-05-06 14:39:53 +0300

    Added CustomEditText with two overridden methods.

--- app/src/main/java/ua/kaganovych/persistencesearch/CustomEditText.java
+++ app/src/main/java/ua/kaganovych/persistencesearch/CustomEditText.java
@@ -1,17 +1,58 @@
 package ua.kaganovych.persistencesearch;
 
 import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 
 public class CustomEditText extends EditText {
 
-    public CustomEditText(Context context) {
-        super(context);
+    public interface onArrowDownListener {
+        boolean onArrowDown();
+        boolean onSearchSubmitted();
+    }
+
+    private onArrowDownListener mCallback;
+
+    public CustomEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setCallback(onArrowDownListener mCallback) {
+        this.mCallback = mCallback;
     }
 
     @Override
     public boolean dispatchKeyEventPreIme(KeyEvent event) {
+        Log.d("TAG", "dispatchKeyEventPreIme(" + event + ")");
+        if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
+            KeyEvent.DispatcherState state = getKeyDispatcherState();
+            if (state != null) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN
+                        && event.getRepeatCount() == 0) {
+                    state.startTracking(event, this);
+                } else if (event.getAction() == KeyEvent.ACTION_UP
+                        && !event.isCanceled() && state.isTracking(event)) {
+                    if (mCallback != null) {
+                        return mCallback.onArrowDown() || super.dispatchKeyEventPreIme(event);
+                    }
+                }
+            }
+        }
+
         return super.dispatchKeyEventPreIme(event);
+    }
+
+    @Override
+    public void onEditorAction(int actionCode) {
+        if (actionCode == EditorInfo.IME_ACTION_SEARCH || actionCode == EditorInfo.IME_ACTION_DONE
+                || actionCode == EditorInfo.IME_ACTION_GO || actionCode == EditorInfo.IME_ACTION_NONE) {
+            if (!TextUtils.isEmpty(getText().toString())) {
+                mCallback.onSearchSubmitted();
+            }
+        }
     }
 }
--- app/src/main/java/ua/kaganovych/persistencesearch/MainActivity.java
+++ app/src/main/java/ua/kaganovych/persistencesearch/MainActivity.java
@@ -1,27 +1,26 @@
 package ua.kaganovych.persistencesearch;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity{
+public class MainActivity extends ActionBarActivity {
 
-    private EditText mSearch;
-    private ImageView mSearchIcon;
+    private CustomEditText mSearch;
+    private ImageView mHamArrowIcon;
     private RelativeLayout mMainLayout;
     private CustomListView mListView;
     private CustomAdapter mAdapter;
@@ -33,12 +32,34 @@ public class MainActivity extends ActionBarActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSearch = (EditText) findViewById(R.id.search);
-        mSearchIcon = (ImageView) findViewById(R.id.hamArrowIcon);
+        mSearch = (CustomEditText) findViewById(R.id.search);
+        mHamArrowIcon = (ImageView) findViewById(R.id.hamArrowIcon);
         mMainLayout = (RelativeLayout) findViewById(R.id.mainLayout);
         mListView = (CustomListView) findViewById(R.id.listView);
         mClear = (ImageView) findViewById(R.id.clear);
         mClear.setVisibility(View.GONE);
+
+        Button open = (Button)findViewById(R.id.buttonOpen);
+        Button close = (Button)findViewById(R.id.buttonClose);
+
+        open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListView.setVisibility(View.VISIBLE);
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+            }
+        });
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListView.setVisibility(View.GONE);
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+            }
+        });
+
 
         mList = new ArrayList<>();
         mList.add(new Item("One"));
@@ -56,7 +77,6 @@ public class MainActivity extends ActionBarActivity{
         mListView.setAdapter(mAdapter);
         mListView.setVisibility(View.GONE);
 
-
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
@@ -66,14 +86,6 @@ public class MainActivity extends ActionBarActivity{
                 hideKeyboard(view);
             }
         });
-
-//        mMainLayout.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View view, MotionEvent motionEvent) {
-//                hideKeyboard(view);
-//                return false;
-//            }
-//        });
 
         mClear.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -113,20 +125,24 @@ public class MainActivity extends ActionBarActivity{
             }
         });
 
-        mSearchIcon.setOnClickListener(new View.OnClickListener() {
+        mSearch.setCallback(new CustomEditText.onArrowDownListener() {
             @Override
-            public void onClick(View view) {
-                hideKeyboard(view);
+            public boolean onArrowDown() {
+                hideKeyboard(mSearch);
+                return true;
+            }
+
+            @Override
+            public boolean onSearchSubmitted() {
+                hideKeyboard(mSearch);
+                return true;
             }
         });
 
-        mSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        mHamArrowIcon.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-//                if (i == EditorInfo.IME_ACTION_SEARCH) {
-//                    mSearch.clearFocus();
-//                }
-                return true;
+            public void onClick(View view) {
+                hideKeyboard(view);
             }
         });
     }
